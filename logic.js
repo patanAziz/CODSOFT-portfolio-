f305cda8925fca5276274830808e7716b74d450b
patanAziz <patanabdulaziz73@gmail.com>
2024-05-25 01:49:14 +0530

    commit

--- logic.js
+++ logic.js
@@ -3,11 +3,12 @@ let a= document.querySelector(".hamburg");
 a.addEventListener("click",myfunction);
 function myfunction(){
     document.querySelector("#h").style.top='0';
+
 }
 
 
 let b= document.querySelector(".contentdev");
 b.addEventListener("click",myfunction1);
 function myfunction1(){
-    document.querySelector("#h").style.left="-600px";
+    document.querySelector("#h").style.left="-1000px";
 }
